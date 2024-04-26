e0a53bf73cbecf69b781b4139f2e4a5c575db231
SlimSim <slimsimapps@gmail.com>
2024-04-26 15:03:14 +0200

    ajusting scale

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=3"
+      ></meta>
       <body>{children}</body>
     </html>
   );
